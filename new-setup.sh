5e06b52874c7b2012f168494bc538aa6ff4a7b18
Ed Rooth <ed.rooth@coreos.com>
2015-11-02 11:49:48 -0800

    add .psqlrc

--- new-setup.sh
+++ new-setup.sh
@@ -35,6 +35,7 @@ ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 mv $HOME/.vim $HOME/.vim_old
 ln -s $HOME/dotfiles/vim $HOME/.vim
 ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/psql/psqlrc $HOME/.psqlrc
 
 echo "change shell to zsh"
 chsh -s $(which zsh)
