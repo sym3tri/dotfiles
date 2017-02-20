de3b06450d94ad7c6d3b3c74df16a397b107abc1
Ed Rooth <ed.rooth@coreos.com>
2017-02-20 15:13:18 -0800

    linux fixes

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd $HOME
-
 echo "setup yum repos"
-
 sudo cat << EOF > /etc/yum.repos.d/google-chrome.repo
 [google-chrome]
 name=google-chrome - \$basearch
@@ -14,11 +11,8 @@ gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
 EOF
 
 echo "dnf install apps"
-sudo dnf install gnome-tweak-tool neovim google-chrome-stable htop powertop paperkey stow mercurial nautilus-dropbox ShellCheck arandr uznip quemu the_silver_searcher tmux 
+sudo dnf install gnome-tweak-tool neovim google-chrome-stable htop powertop paperkey stow mercurial nautilus-dropbox ShellCheck arandr uznip quemu the_silver_searcher tmux
 
-echo "setup dotfiles"
-git clone git@github.com:sym3tri/dotfiles.git
-cd $HOME/dotfiles
 stow bash
 stow git
 stow nvim
@@ -27,6 +21,6 @@ stow terraform
 
 # GO
 wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.8.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.8.linux-amd64.tar.gz
 
 ./go-get.sh
